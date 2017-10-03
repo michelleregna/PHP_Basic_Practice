45f4aab7eae5b0ee9b8ed4516fe7631b3a0fbe85
Michelle Regna <michelleregna@gmail.com>
2017-10-03 17:39:00 -0400

    replace - with / using str_replace function

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 
-$date =  date('Y/m/d', time());
+$date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
-print_r($date);
-echo "<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
-print_r($year)
+print_r($year);
+echo "<br><br>";
 
-?> 
+echo "1." . str_replace("-", "/", $date) . "<br>";
+
+
+
+?>
