5bd48f207fc55318294323826e6cc1f80761b92f
Michelle Regna <michelleregna@gmail.com>
2017-10-03 18:05:59 -0400

    added ord function

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$str = "Hello world";
+
 $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
 
@@ -11,9 +13,9 @@ echo "The value of \$year: ";
 print_r($year);
 echo "<br><br>";
 
-echo "2." . str_replace("-", "/", $date) . "<br>";
+echo "2. The new value of date: " . str_replace("-", "/", $date) . "<br>";
 
-echo "3.";
+echo "3. Comparing \$date and \$tar: ";
 if (strcmp($date, $tar) > 0) {
 	echo "The future";
 } else if (strcmp($date, $tar) < 0) {
@@ -21,6 +23,10 @@ if (strcmp($date, $tar) > 0) {
 } else {
 	echo "Oops";
 }
+
+echo "5." . "<br>";
+echo "6." . "<br>";
+echo "7.The ASCII value of the first character in \$str: " . ord($str) . "<br>";
 
 
 
